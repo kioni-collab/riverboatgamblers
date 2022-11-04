db331d71685cab9214c985ec3a6245b138f4f411
kioni kamau <kkamau@plu.edu>
2022-11-04 00:38:46 -0700

    Feature: Implemented vanila CFR for multiplayer
    Signed-off-by: kioni kamau <kkamau@plu.edu>

--- Poker_Bot/Poker_Bot.py
+++ Poker_Bot/Poker_Bot.py
@@ -9,7 +9,7 @@ from Card import card
 
 NUM_ACTIONS = 4 # Raise,Call,Check,Fold - we change to be number of actions we have from unity 
 NUM_CARDS = 52 # size of the deck
-infoset_map = {}
+infoset_map = {} # this is outside for continous training should also probabily pickle too
 SUIT = ["H","S","D","C"]
 RANK = ["A","1", "2","3","4","5","6","7","8","9","10","J","K","Q"]
 DECK = set()
@@ -52,7 +52,32 @@ class game_node:
         strategies = ['{:03.2f}'.format(x) for x in self.get_average_strategy()]
         return '{} {}'.format(self.key.ljust(6), strategies)
 
-        
+def cfr(history=[], cards = [[-1],[-1],[-1]], pr=[[1],[1],[1]] ,pr_c=1):
+    # investigate pr_c more to see if it changes
+    player_num = 1 # call to unity for which player
+    info_set = get_info_set(cards[player_num], history)
+    strategy = info_set.strategy
+    info_set.reach_pr += pr[player_num]
+    action_utils = np.zeros(NUM_ACTIONS) # change with actions given by unity
+    actions_cur = ["fold","check","raise","call"] # will get from unity this is example
+    for i, action in enumerate( actions_cur):
+        next_history = history.append(action)
+        pr_next  = list(pr)
+        pr_next[player_num] = pr_next[player_num] * strategy[i] 
+        action_utils[i] = -1 * cfr( next_history,cards,pr_next, pr_c)
+    util = sum(action_utils * strategy)
+    regrets = action_utils - util
+    info_set.regret_sum += (np.prod(pr)/pr[player_num]) * pr_c * regrets
+    return util
+    
+def get_info_set(cards,history):
+    key = str(cards) + " " + str(history)
+    info_set = None
+    if key not in infoset_map:
+      info_set = game_node(key)
+      infoset_map[key] = info_set
+      return info_set
+    return infoset_map[key]
 
 def __main__():
     # most of this will replaced with a call to unity 
