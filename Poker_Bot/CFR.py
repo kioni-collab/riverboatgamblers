99d1486f3faebe9f2e3a1f3fad82df99da317087
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2022-11-22 13:36:40 -0800

    Fixed: m_v is nested list, added models varibles
    fixed the traversal to hopefully work, and added models which will be
    trained later. gotta figure out the interpations for this and seperate
    infoset to be cards and betting
    
    Signed-off-by: kioni-collab <86212205+kioni-collab@users.noreply.github.com>

--- Poker_Bot/CFR.py
+++ Poker_Bot/CFR.py
@@ -1,19 +1,22 @@
 from Model import DeepCFRModel 
 class CFR():
     def __init__(self,players):
-        self.m_v = [DeepCFRModel for p in range(len(players))]
-        self.m_p = DeepCFRModel
+        self.m_v = [[] for p in range(len(players))]
+        self.m_p = []
+        self.value_model = DeepCFRModel()
+        self.strat_model = DeepCFRModel()
 
 
-    def deepcfr(T,player,K):
+    def deepcfr(self,T,player,K):
         for t in range(1000):
             for p in range(len(player)):
                 for k in range(K):
-                    
-                    traversal([],p,p.cards(),) 
-                    
+                    self.traversal([],p,[pl.card() for pl in player], self.m_v[p], self.m_p)
+                # train model  
 
+        # train model b 
+        
+        pass            
 
-
-    def traversal(h =[],p,cards,m_v,m_p,t):
+    def traversal(self,h =[],p,cards,m_v,m_p,t):
         pass
