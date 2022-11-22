644ffb226031afeb5dada81800fea5764562df5a
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2022-11-22 14:17:04 -0800

    comment

--- Poker_Bot/CFR.py
+++ Poker_Bot/CFR.py
@@ -12,18 +12,17 @@ class CFR():
     # may need to change how we pass network parametrs through traversal
     # may need infoset map as well
     # find out if models are already initialized to return 0 
-
-
     def deepcfr(self,T,player,K):
         for t in range(1000):
             for p in range(len(player)):
                 for k in range(K):
                     self.traversal([],p,[pl.card() for pl in player], self.m_v[p], self.m_p)
-                # train model  
+                # train model 
+                 
 
         # train model b 
         
         pass            
 
-    def traversal(self,h = [], p ,cards,m_v,m_p,t):
+    def traversal(self,h,p cards,m_v,m_p,t):
         pass
