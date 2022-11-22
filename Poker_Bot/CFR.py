08da6a521d4aee654b4025068858f2420e040c34
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2022-11-22 13:53:50 -0800

    TODO: added comments on what to do next
    Signed-off-by: kioni-collab <86212205+kioni-collab@users.noreply.github.com>

--- Poker_Bot/CFR.py
+++ Poker_Bot/CFR.py
@@ -7,6 +7,13 @@ class CFR():
         self.strat_model = DeepCFRModel()
 
 
+    # TODO 
+    # figure out how to split infoset to be cards in hand, cards on board and betting postions
+    # may need to change how we pass network parametrs through traversal
+    # may need infoset map as well
+    # find out if models are already initialized to return 0 
+
+
     def deepcfr(self,T,player,K):
         for t in range(1000):
             for p in range(len(player)):
@@ -18,5 +25,5 @@ class CFR():
         
         pass            
 
-    def traversal(self,h =[],p,cards,m_v,m_p,t):
+    def traversal(self,h = [], p ,cards,m_v,m_p,t):
         pass
