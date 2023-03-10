8f5b5712990b9144f2865d38b5f99b96cc92c73b
kioni kamau <kkamau@plu.edu>
2023-03-10 04:41:26 -0600

    hopefully good now

--- Poker_Bot/CFR.py
+++ Poker_Bot/CFR.py
@@ -1,8 +1,9 @@
 from Model import DeepCFRModel 
-from Infoset import game_node
 import numpy as np 
 from random import choices
+import random
 from torch import optim
+from player import Player
 class CFR():
     def __init__(self,players):
         self.m_v = [[] for p in range(len(players))]
@@ -17,11 +18,12 @@ class CFR():
     # may need to change how we pass network parametrs through traversal
     # may need infoset map as well
     # find out if models are already initialized to return 0 
-    def deepcfr(self,T,player,K,board:list):
+    def deepcfr(self,T,players,K,board:list):
         for t in range(1000):
-            for p in range(len(player)):
+            for p in range(len(players)):
                 for k in range(K):
-                    self.traversal([],p,[pl.card() for pl in player],board,self.m_v[p], self.m_p)
+
+                    self.traversal([],p,players,board, 10,20,0)
                 # train model 
                 self.value_model = DeepCFRModel()
                 optimizer = optim.Adam(self.value_model.parameters())
@@ -58,53 +60,86 @@ class CFR():
         pass            
 
 # g is actions i that round
-    def traversal(self,h,p ,cards,board,t,g, round):
-        if self.is_terminal(h,round, len(cards) ):
-            return self.util(h,p,cards,board)
+    def traversal(self,h,p ,players,board,t,cur_bet, min_bet,max_bet,round,deck):
+        if self.is_terminal(round, len(players) ):
+            return self.util(h,p,players,board)
         # what does a chance node do in poker, it is adding a new card to the board
-        # so we have to simulate that, its actions are not fold check raise
-        elif self.is_chance(g, len(cards)):
+        elif self.is_chance(players):
             # get number of cards left in deck
             # pick one randomly
             # add to board and history
             # done 
-            g_new = []
+            board.append(deck.pop(random.randrange(len(deck))))
+            h.append("board_card")
+            #reset all player actions to none
+            new_players = list(players)
+            for pl in new_players:
+                pl.set_last_action(None) 
+
             round += 1
-            return 
-        elif self.current_player(g) == p:
+            return self.traversal(h,p ,new_players,board,t,cur_bet, min_bet,max_bet,round,deck) 
+        
+        elif self.current_player(players).ID() == p:
             # make a regret matching function
             #
             o = 0 # this will be generated from neural network
             v = {}
             v_o = 0
             r = {}
-            for a in self.get_actions(g,p):
+            for a in self.get_actions(players,round):
                 h_new = list(h)
                 h_new.append(a)
-                g_new = list(g)
-                g_new.append(a)
-                v[a] = self.traversal(h_new,p,cards,board,t, g_new)
+                new_players = list(players)
+                new_players[0].set_last_action(a)
+                if a == "bet":
+                    if round < 3:
+                        cur_bet += min_bet
+                    else:
+                        cur_bet += max_bet
+                    new_players[0].set_bet_amt(cur_bet)
+                elif a == "call":
+                    new_players[0].set_bet_amt(cur_bet)
+                
+                if a == "fold":
+                    new_players = new_players[1:]
+                else:
+                    new_players = new_players[1:] + new_players[0]
+
+                v[a] = self.traversal(h_new,p,new_players,board,t,cur_bet, min_bet, max_bet, round)
                 v_o += o[a] * v[a]
             for a in self.get_actions(h):
                 #work out what data type r(I,a) should be
                 r[a] = v[a] - v_o
                 pass
-            self.m_v.append(([cards[p],h,board],t,r))
+            self.m_v.append(([players[p].get_cards(),h,board],t,r))
             return v_o 
             #insert into m_V
         else:
             # Insert the infoset and its action probabilities (I, t, σt(I)) into the strategy memory MΠ
             # edit the regret_matching stuff so it uses the right cards
-            cards_not_p = list(cards).pop(p)
-            o = self.regret_matching(h,p,cards_not_p,board)
-            self.m_p.append(([cards[p],h,board],t,o))
+            o = None # add neural network
+            self.m_p.append(([players[0].get_cards(),h,board],t,o))
             A = self.get_actions(h)
             a = choices(A,o)
             h_new = list(h)
             h_new.append(a)
-            g_new = list(g)
-            g_new.append(a)
-            return self.traversal(h_new,p,cards,board,t, g_new)
+            new_players = list(players)
+            new_players[0].set_last_action(a)
+            if a == "bet":
+                if round < 3:
+                    cur_bet += min_bet
+                else:
+                    cur_bet += max_bet
+                new_players[0].set_bet_amt(cur_bet)
+            elif a == "call":
+                new_players[0].set_bet_amt(cur_bet)
+            
+            if a == "fold":
+                new_players = new_players[1:]
+            else:
+                new_players = new_players[1:] + new_players[0]
+                
+            return self.traversal(h_new,p,new_players,board,t,cur_bet, min_bet,max_bet,round)
 
             pass
 
@@ -112,26 +147,16 @@ class CFR():
         
         pass
     
-    def is_terminal(self,h, round, player_num):
+    def is_terminal(self,round, player_num):
         return (round == 4) or player_num == 1
     
     def util(self,h,p,cards,board):
         # call to unity for money won
         pass
     #assume that elemts in g are like {"player_num": 3, action:"check"}
-    def is_chance(self,g,player_num):
-        equal_bet_list = []
-        check_list = []
-        for a in g:
-            if a["action"] == "bet":
-                check_list.clear()
-                equal_bet_list.clear()
-                equal_bet_list.append(a)
-            if a["action"] == "call":
-                equal_bet_list.append(a)
-            if a["action"] == "check":
-                check_list.append(a)
-        return (len(equal_bet_list) == player_num) or (len(check_list) == player_num)
+
+    def is_chance(self,players):
+        return  all([(p.get_bet_amt() == players[0].get_bet_amt() and players[0].get_bet_amt() != 0 and p.get_last_action() is not None) for p in players]) or all([p.last_action() == "check" for p in players])
 
 
 
@@ -139,21 +164,13 @@ class CFR():
 # for players we check we put them at the back of the list so the players who havent had a turn yet are at the front
 #we return the first player in the queue 
 #review to see if makes sense 
-    def current_player(g,player_num):
-        player_q = [i for i in range(player_num)]
-        for l in g:
-            if l["action"] != "Check":
-                player_q.remove[l["player"]]
-            else:
-                player_q.remove[l["player"]]
-                player_q.append[l["player"]]
-
-        return player_q[0]
+    def current_player(players):
+        return players[0]
     
-    def get_actions(g,p):
-        if any([l["action"] == "bet " for l in g]):
+    def get_actions(players,round):
+        if round == 1:
+            return ["bet","fold"]
+        if any([p.get_last_action() == "bet" for p in players]):
             return ["bet","call", "fold"]
         else:
-            return ["bet","check", "fold"]
-        
-        
+            return ["bet", "check", "fold"]
